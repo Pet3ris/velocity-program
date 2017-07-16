fc774e4ba55c3d3f807b33a6c7978e80337d324e
Peteris Erins <peteris.erins@gmail.com>
2017-07-16 10:56:50 +0100

    deletes

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-
-app.listen(process.env.PORT || 5000, function() {
-  console.log('Listening on port 5000.');
-});
